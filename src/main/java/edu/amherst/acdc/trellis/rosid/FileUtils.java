bdadd5215bb685d72cdd1b62c9dd4065d9e8d846
Aaron Coburn <acoburn@amherst.edu>
2017-02-04 17:21:17 -0500

    add better partitioner and tests

--- src/main/java/edu/amherst/acdc/trellis/rosid/FileUtils.java
+++ src/main/java/edu/amherst/acdc/trellis/rosid/FileUtils.java
@@ -17,9 +17,10 @@ package edu.amherst.acdc.trellis.rosid;
 
 import static java.io.File.separator;
 import static java.util.Objects.requireNonNull;
+import static java.util.stream.IntStream.range;
 import static org.apache.commons.codec.digest.DigestUtils.md5Hex;
 
-import java.io.File;
+import java.util.StringJoiner;
 import java.util.zip.CRC32;
 
 import org.apache.commons.rdf.api.IRI;
@@ -29,22 +30,23 @@ import org.apache.commons.rdf.api.IRI;
  */
 class FileUtils {
 
-    public static File getResourceDirectory(final File baseDir, final IRI identifier) {
-        requireNonNull(baseDir, "baseDir must be nonnull!");
+    // The length of the CRC directory partition
+    public final static int LENGTH = 2;
+
+    public static String partition(final IRI identifier) {
         requireNonNull(identifier, "identifier must not be null!");
 
+        final StringJoiner joiner = new StringJoiner(separator);
         final CRC32 hasher = new CRC32();
         hasher.update(identifier.getIRIString().getBytes());
-        final String intermediateDir = Long.toHexString(hasher.getValue());
-        final String path = md5Hex(identifier.getIRIString());
-        final StringBuilder builder = new StringBuilder();
-        for (int i = 0; i < intermediateDir.length() / 2; ++i) {
-            builder.append(intermediateDir.substring(i, i + 2));
-            builder.append(separator);
-        }
-        final File resourceDir = new File(baseDir, builder.toString() + path);
-        resourceDir.mkdirs();
-        return resourceDir;
+        final String intermediate = Long.toHexString(hasher.getValue());
+
+        final int count = intermediate.length() / LENGTH;
+
+        range(0, count).forEach(i -> joiner.add(intermediate.substring(i * LENGTH, (i + 1) * LENGTH)));
+
+        joiner.add(md5Hex(identifier.getIRIString()));
+        return joiner.toString();
     }
 
     private FileUtils() {

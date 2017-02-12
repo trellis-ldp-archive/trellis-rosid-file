aee9d28f03f2921017e0b82df7f6d3625ab20087
Aaron Coburn <acoburn@amherst.edu>
2017-02-12 06:50:42 -0500

    simplify

--- src/main/java/edu/amherst/acdc/trellis/rosid/RDFPatch.java
+++ src/main/java/edu/amherst/acdc/trellis/rosid/RDFPatch.java
@@ -17,28 +17,42 @@ package edu.amherst.acdc.trellis.rosid;
 
 import static java.lang.String.join;
 import static java.lang.System.lineSeparator;
+import static java.nio.file.Files.lines;
 import static java.nio.charset.StandardCharsets.UTF_8;
 import static java.nio.file.Files.newBufferedWriter;
 import static java.nio.file.StandardOpenOption.APPEND;
 import static java.time.Instant.now;
+import static java.time.Instant.parse;
 import static java.util.stream.StreamSupport.stream;
+import static org.apache.commons.rdf.jena.JenaRDF.asTriple;
+import static org.apache.jena.riot.Lang.NTRIPLES;
+import static org.apache.jena.riot.system.StreamRDFLib.sinkTriples;
 
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.IOException;
+import java.io.StringReader;
 import java.io.UncheckedIOException;
 import java.io.Writer;
 import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Iterator;
 import java.util.stream.Stream;
 
 import org.apache.commons.rdf.api.Graph;
 import org.apache.commons.rdf.api.RDF;
 import org.apache.commons.rdf.api.Triple;
+import org.apache.jena.atlas.lib.SinkToCollection;
+import org.apache.jena.riot.RDFParserRegistry;
+import org.apache.jena.riot.ReaderRIOT;
 
 /**
  * @author acoburn
  */
 class RDFPatch {
+
+    private static final ReaderRIOT READER = RDFParserRegistry.getFactory(NTRIPLES).create(NTRIPLES);
 
     /**
      * Read the triples from the journal that existed up to (and including) the specified time
@@ -64,8 +78,27 @@ class RDFPatch {
     }
 
     public static Graph asGraph(final RDF rdf, final File file, final Instant time) {
-        final RDFPatchGraphReader reader = new RDFPatchGraphReader(rdf, file, time);
-        return reader.getGraph();
+        final Graph graph = rdf.createGraph();
+        try {
+            final Iterator<String> allLines = lines(file.toPath()).iterator();
+            while (allLines.hasNext()) {
+                final String line = allLines.next();
+                if (line.startsWith("D ANY ANY ANY")) {
+                    graph.clear();
+                } else if (line.startsWith("BEGIN # ")) {
+                    if (time.isAfter(parse(line.split(" # ", 2)[1]))) {
+                        break;
+                    }
+                } else if (line.startsWith("A ")) {
+                    graph.add(stringToTriple(rdf, line.split(" ", 2)[1]));
+                } else if (line.startsWith("D ")) {
+                    graph.remove(stringToTriple(rdf, line.split(" ", 2)[1]));
+                }
+            }
+        } catch (final IOException ex) {
+            throw new UncheckedIOException(ex);
+        }
+        return graph;
     }
 
     /**
@@ -135,6 +168,13 @@ class RDFPatch {
         } catch (final IOException ex) {
             throw new UncheckedIOException(ex);
         }
+    }
+
+    private static Triple stringToTriple(final RDF rdf, final String line) {
+        final List<org.apache.jena.graph.Triple> c = new ArrayList<>();
+        READER.read(new StringReader(line), null, NTRIPLES.getContentType(),
+                sinkTriples(new SinkToCollection<>(c)), null);
+        return asTriple(rdf, c.get(0));
     }
 
     private RDFPatch() {

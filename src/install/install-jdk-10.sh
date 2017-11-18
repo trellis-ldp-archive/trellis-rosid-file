c0a5a3bc03576118689b8bf7d6b2323a6633c2ed
Aaron Coburn <acoburn@amherst.edu>
2017-11-18 16:51:32 -0500

    Get the most recent build number automatically

--- src/install/install-jdk-10.sh
+++ src/install/install-jdk-10.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
+
+# Based on JDK 10 installation script from the junit5 project
+
 set -e
 
 JDK_FEATURE=10
-JDK_BUILD=32
+TMP=$(curl -L jdk.java.net/${JDK_FEATURE})              # extract most recent "JDK_BUILD" number from web page source
+TMP="${TMP#*Most recent build: jdk-${JDK_FEATURE}-ea+}" # remove everything before the number
+TMP="${TMP%%<*}"                                        # remove everything after the number
+JDK_BUILD="$(echo -e "${TMP}" | tr -d '[:space:]')"     # remove all whitespace
 JDK_ARCHIVE=jdk-${JDK_FEATURE}-ea+${JDK_BUILD}_linux-x64_bin.tar.gz
 
 cd ~
